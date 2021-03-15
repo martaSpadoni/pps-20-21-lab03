c64cbf347a889cb3dfea86822159367e8c7b672d
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-15 11:45:19 +0100

    refactoring drop function

--- src/lab03/ListFunctions.scala
+++ src/lab03/ListFunctions.scala
@@ -1,12 +1,15 @@
 package lab03
 
+import scala.annotation.tailrec
+
 object ListFunctions {
 
   import u03.Lists._
   import List._
 
+  @tailrec
   def drop[A](l: List[A], n: Int): List[A] = l match {
-    case Cons(h, t) if n > 0 => drop(t, n-1)
+    case Cons(_, t) if n > 0 => drop(t, n-1)
     case _ => l
   }
 
